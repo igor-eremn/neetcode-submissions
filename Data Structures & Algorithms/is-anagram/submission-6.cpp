ec87b75ef4f28df88df41dc1aabe26d0d8cc03b3
i.g.o.r. <77459655+igor-eremn@users.noreply.github.com>
2026-07-03 20:07:11 -0700

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size())
+        {
+            return false;
+        }
+
+        std::unordered_map<char, int> map{};
+
+        for (int i=0; i<s.size(); i++)
+        {
+            map[s[i]]++;
+        }
+        
+        for (int i=0; i<t.size(); i++)
+        {
+            map[t[i]]--;
+            if (map[t[i]] < 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
