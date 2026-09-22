5e020aba3471dead744d978b7650e17f437549f8
i.g.o.r. <77459655+igor-eremn@users.noreply.github.com>
2026-09-21 18:53:17 -0700

    Add: string-encode-and-decode - submission-11

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-11.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string res;
+        for (int i=0; i<strs.size(); i++){
+            res += to_string(strs[i].size()) + "#";
+            res += strs[i];
+        }
+        cout << res << "\n";
+        return res;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> res;
+        string num;
+        for (int i=0; i<s.length(); i++){
+            if (s[i] != '#'){
+                num += s[i];
+            } else {
+                i++;
+                int len = stoi(num);
+                string temp = s.substr(i, len);
+                res.push_back(temp);
+                i += len - 1;
+                num = "";
+            }
+        }
+
+        return res;
+    }
+};
